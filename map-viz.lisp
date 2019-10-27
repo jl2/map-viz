d0941c2c95a95d009feb8cb0f7e71a3155f7cc37
Jeremiah LaRocco <jeremiah_larocco@fastmail.com>
2019-10-26 22:00:16 -0600

    Initial (broken) implementation of dataset bounding box visualization.

--- map-viz.lisp
+++ map-viz.lisp
@@ -17,20 +17,39 @@
 
 (in-package :map-viz)
 
-(defun show-bounding-boxes (&key shape-file-name output-file-name)
+(defun show-bounding-boxes (&key
+                              (shape-file-name)
+                              (output-file-name)
+                              (width 1600)
+                              (height 1600)
+                              (image-type "BMP"))
   (declare (ignorable output-file-name shape-file-name))
+  (ensure-directories-exist output-file-name)
 
-  (gdal:gdal-all-register)
-  (loop
-     with dataset = (gdal:gdal-open-ex shape-file-name gdal:+of-vector+ +np+ +np+ +np+)
-     for i below (gdal:ogr-ds-get-layer-count dataset)
-     for layer = (gdal:ogr-ds-get-layer dataset i)
-     for layer-name = (gdal:ogr-l-get-name layer)
-     do
-       (format t "~a has ~a features.~%" layer-name (gdal:ogr-l-get-feature-count layer 1))
-       (multiple-value-bind (min-x max-x min-y max-y) (gdal:get-layer-envelope layer)
-          (format t
-                  "Layer ~a bounding box is: (~a ~a) - (~a ~a)~%"
-                  layer-name min-x min-y max-x max-y)
-          (autowrap:free layer))
-     finally (autowrap:free dataset)))
+  (bl:with-image-context*
+      (img ctx output-file-name
+           :width width
+           :height height
+           :codec-name image-type)
+      ((rect bl:rect))
+
+    (gdal:gdal-all-register)
+    (let ((dataset (gdal:gdal-open-ex
+                    shape-file-name
+                    gdal:+of-vector+
+                    gdal:+np+ gdal:+np+ gdal:+np+)))
+      (format t "dataset: ~a~%" dataset)
+      (multiple-value-bind (x-min x-max y-min y-max) (gdal:get-dataset-envelope dataset)
+        (format t "x-min ~a y-min ~a x-max ~a y-max ~a w ~a h ~a~%" x-min y-min x-max y-max (- x-max x-min) (- y-max y-min))
+        (bl:setup-window ctx :width width :height height :x-min x-min :y-min y-min :x-max x-max :y-max y-max :stroke-width 10.0)
+        (setf (bl:rect.x rect) x-min)
+        (setf (bl:rect.y rect) y-min)
+        (setf (bl:rect.w rect) (- x-max x-min))
+        (setf (bl:rect.h rect) (- y-max y-min))
+        (bl:context-set-comp-op ctx bl:+comp-op-src-over+)
+        (bl:context-set-stroke-style-rgba32 ctx #16rffffffff)
+        ;; (bl:context-stroke-geometry ctx bl:+geometry-type-line+ line)
+        (bl:context-set-stroke-width ctx 20.0)
+        (bl:lookup-error (bl:context-stroke-geometry ctx bl:+geometry-type-rectd+ rect)))
+      ;; (autowrap:free dataset)
+      )))
